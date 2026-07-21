b03235bc6e15f143c481e1ec9478c0b19a890c9f
LuVisage <1977928878@qq.com>
2026-07-21 18:15:54 +0800

    🐛 修复 MDX 表格渲染：添加 remark-gfm 支持 GFM 表格语法

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,6 +1,7 @@
 import { MDXRemote } from 'next-mdx-remote-client/rsc'
 import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
+import remarkGfm from 'remark-gfm'
 import { useMDXComponents } from './mdx-components'
 
 interface MDXContentProps {
@@ -16,6 +17,7 @@ export function MDXContent({ source }: MDXContentProps) {
       components={components}
       options={{
         mdxOptions: {
+          remarkPlugins: [remarkGfm],
           rehypePlugins: [
             rehypeSlug,
             [
